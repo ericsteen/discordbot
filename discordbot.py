673ecd9fe713ea4891a7a7f1e1e79f75c4b8f72f
Eric Steen <eric@steen.run>
2023-07-11 23:10:02 -0500

    first commit

---
+++ discordbot.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import discord
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:XXXXXXXXX' #whatever 
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
+
+dotaUpdateUrl = "https://isthepatchout.com"
+
+print(dotaUpdateUrl)
+
+
+
+def SpotSearch():
+
+def SpotDisplay():
+
+
