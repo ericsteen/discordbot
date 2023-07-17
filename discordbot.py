df826c28730c838564439b6985d8e968edf51277
Eric Steen <eric@steen.run>
2023-07-16 23:22:38 -0500

    works

--- discordbot.py
+++ discordbot.py
@@ -1,45 +1,29 @@
-1import os
-import requests
+import os
+#import requests
 import discord
 import responses
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import ytmusic
 
 dotaUpdateUrl = "https://isthepatchout.com"
 
 print(dotaUpdateUrl)
-
-ytdl_opts={
-    'format': 'bestaudio/best',
-    'outtmpl': 'tmp/%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'default_search':'auto',
-    'source_address': '0.0.0.0'
-        }
-ytdl = YoutubeDL(ytdl_opts)
-
 
 def ReadTokens():
     TOKEN = os.getenv('DISCORD_TOKEN')
     ID = os.getenv('DISCORD_ID')
     return TOKEN, ID
 
-
-def SpotSearch():
-    pass
-def SpotDisplay():
-    pass
-
 def runDiscordBot():
     clientToken, clientID = ReadTokens()
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
+
+    yt-music.MusicPlayer(client)
+
 
     @client.event
     async def on_ready():
@@ -59,8 +43,8 @@ def runDiscordBot():
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-            if user_message.split(" ")[0] == '!play':
-                ytdl.ytdl(username, user_message, channel)
+        if user_message.split(" ")[0] == '!play':
+            ytdl.ytdl(username, user_message, channel)
         if user_message == "$devices" and message.author.voice:
             voice_client = discord.utils.get(client.voice_clients)
             audio_sources = voice_client.source.audio_source
