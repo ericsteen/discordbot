649d8234c251267539c5386b3dbcc8fa7094f28b
Eric Steen <eric@steen.run>
2023-07-18 16:07:11 -0500

    works

--- discordbot.py
+++ discordbot.py
@@ -1,74 +1,65 @@
-import os
-#import requests
 import discord
 from discord.ext import commands
-import responses
-from dotenv import load_dotenv
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import ytmusic
+import yt_dlp
+import os
+import dotenv
+import subprocess
 
-dotaUpdateUrl = "https://isthepatchout.com"
+yt_opts = {
+    'format': 'bestaudio', 
+    'extract-audio': True,
+    'output': ''
+        }
 
-print(dotaUpdateUrl)
 
-def ReadTokens():
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    ID = os.getenv('DISCORD_ID')
-    return TOKEN, ID
-
+TOKEN = os.getenv('DISCORD_TOKEN')
 def runDiscordBot():
-    clientToken, clientID = ReadTokens()
     intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intents)
     bot = commands.Bot(command_prefix='!', intents=intents)
 
-    ytmusic.Music(bot)
 
-
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is online')
+        print(f'{bot.user.name} has arrived')
 
-    async def send_message(message, user_message):
-        try:
-            response = responses.get_responses(user_message)
-            await message.channel.send(response)
-        except Exception as e:
-            print(e)
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        #if user_message.split(" ")[0] == '!play':
-            #ytdl.ytdl(username, user_message, channel)
-        if user_message == "$devices" and message.author.voice:
-            voice_client = discord.utils.get(client.voice_clients)
-            audio_sources = voice_client.source.audio_source
-            print("AUDIO_SOURCES: ")
-            for audio_source in audio_sources:
-                print(audio_source.title)
-            
-            audio_sources = voice_client.source.audio_source
-        print(f'bro really said "{user_message}" ({channel})')
-        await send_message(message, user_message)
+    @bot.command()
+    async def play(ctx, url):
+        if ctx.author.voice:
+            voice_channel = ctx.author.voice.channel
+            voice_client = await voice_channel.connect()
+            print('joined voice channel')
+        else:
+            print('failed to join voice channel')
 
 
-    @client.event
-    async def on_member_join(member):
-        print(f'{member} joined')
-        await message.channel.send('{member} has joined. Another wanderer, here to lick my father\'s boots. Good job')
+        ytdl = yt_dlp.YoutubeDL(yt_opts)
+        info = ytdl.extract_info(url, download=False)
+        print(info)
+        audio_url = info['formats'][0]['url']
 
-        
-    print(clientToken)
-    client.run(clientToken)
+        print(url)
+        print(audio_url)
+              #yt_dlp_info    = subprocess.Popen([
+        yt_dlp_process = subprocess.Popen(['yt-dlp','-f','ba','-o','-', audio_url], stdout=subprocess.PIPE)
+        bot_audio = discord.FFmpegPCMAudio(yt_dlp_process.stdout)
+        voice_client.play(bot_audio)
 
+        #ytdl = yt_dlp.YoutubeDL(yt_opts)
+        #info = ytdl.extract_info(url, download=False)
+        #audio_url = info['formats'][0]['url']
+        #breakpoint()
+        #voice_client.play(discord.FFmpegPCMAudio(audio_url))
+        #stream = ytdl.download(url)
+        #voice_client.play(discord.FFmpegOpusAudio(stream, *, executable='ffmpeg', pipe=True))
 
+    @bot.command()
+    async def stop(ctx):
+        voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        if voice_client:
+            await voice_client.disconnect()
+
+    bot.run(TOKEN)
 
 if __name__ == '__main__':
     runDiscordBot()
