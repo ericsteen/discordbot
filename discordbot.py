5b24135c4e43b5c7bc9eecc1fd7f730af67ce0d2
Eric Steen <eric@steen.run>
2023-07-17 23:42:54 -0500

    works

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 import os
 #import requests
 import discord
+from discord.ext import commands
 import responses
 from dotenv import load_dotenv
 import spotipy
@@ -21,8 +22,9 @@ def runDiscordBot():
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
-    yt-music.MusicPlayer(client)
+    ytmusic.Music(bot)
 
 
     @client.event
@@ -43,8 +45,8 @@ def runDiscordBot():
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-        if user_message.split(" ")[0] == '!play':
-            ytdl.ytdl(username, user_message, channel)
+        #if user_message.split(" ")[0] == '!play':
+            #ytdl.ytdl(username, user_message, channel)
         if user_message == "$devices" and message.author.voice:
             voice_client = discord.utils.get(client.voice_clients)
             audio_sources = voice_client.source.audio_source
